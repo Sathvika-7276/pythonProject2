9971e0aa342aef960c85c4bba91c00e0c26fa235
Sathvika-7284 <sathvika7276@gmail.com>
2022-11-14 16:24:38 +0800

    Refactor password check program to use functions

--- password_stars.py
+++ password_stars.py
@@ -1,6 +1,16 @@
 minimum_set_length = 4
-user_password = input("Enter password:")
-while len(user_password) < minimum_set_length:
-    print(f"Error! Enter minimum a {minimum_set_length} digit password")
+
+
+def get_password():
     user_password = input("Enter password:")
-print("*" * len(user_password))
+    while len(user_password) < minimum_set_length:
+        print(f"Error! Enter minimum a {minimum_set_length} digit password")
+        user_password = input("Enter password:")
+    print_asterisk(user_password)
+
+
+def print_asterisk(user_password):
+    print("*" * len(user_password))
+
+
+get_password()
